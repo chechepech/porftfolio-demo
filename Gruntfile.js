f5ac32278ed023a982261ab0876676c9fa3ad076
chechepech <bboy_rys@yahoo.com.mx>
2019-12-13 17:31:35 -0600

    portfolio

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            files: 'app/scss/**/*.scss',
+            tasks: ['sass']
+        },
+        sass: {
+            dev: {
+                
+                files: {
+                    'app/css/app.css': 'app/scss/app.scss'
+                }
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'app/css/*.css',
+                        '*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './'
+                }
+            }
+        }
+    });
+
+    // load npm tasks
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    // define default task
+    grunt.registerTask('default', ['browserSync', 'watch']);
+};
